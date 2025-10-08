f9c70251803021bfd0286f85e985de93c442f8a0
Sayyad sheksha <syedshaikshavali@gmail.com>
2025-10-08 19:09:41 +0530

    Deployement edits-3

--- backend/src/server.js
+++ backend/src/server.js
@@ -16,8 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 const allowedOrigins = [
-  "http://localhost:5173",
-  "https://chat-app-1-wine.vercel.app/", // your frontend domain
+  "https://chat-app-1-wine.vercel.app", // your frontend domain
 ];
 app.use(
   cors({
